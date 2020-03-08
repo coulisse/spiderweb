a1155fea2feecaf443094c3ac19654ff4df28c2c
coulisse <corrado.gerbaldo@gmail.com>
2020-03-08 07:48:40 +0000

    added band filters

--- static/js/application.js
+++ static/js/application.js
@@ -1,48 +1,52 @@
-// Builds the HTML Table.
+function findCountry(countries, wpx_to_find) {
+
+	for (var i = 0; i < countries.length; i++) {
+    		if (countries[i].WPX == wpx_to_find) {
+        		return countries[i];
+    			};
+	};
+
+};
+
 function transcodeArea(info){
 	if (info != undefined) {
 		var new_info=info;
-		new_info.areacode=new_info.areacode.replace('UK','GB');
-		new_info.areacode=new_info.areacode.replace('FL','FI');
+		if (new_info.areacode != undefined) {
+			new_info.areacode=new_info.areacode.replace('UK','GB');
+			new_info.areacode=new_info.areacode.replace('FL','FI');
+		};
 		return new_info;
 	} else {
 		return undefined;
 	};
 };
-function buildHtmlTable(selector,data,rl) {
+function buildHtmlTable(selector,data,rl,countries) {
 	//rows
 	var myRows=new Array();
 
 	$(selector).empty();
 	for (var i = 0; i < data.length; i++) {
 
-                myRows[i]=data[i].rowid;
-		var row$;	
-		row$=$('<tr/>');
-		let found = rl.find(element => element ==data[i].rowid);
+         myRows[i]=data[i].rowid;
+		var row$=$('<tr id="'+data[i].rowid+'"/>');
+		var found = rl.find(element => element ==data[i].rowid);
 		if ( found == undefined && rl.length > 0) {
-			row$=$('<tr class="table-info"/>');
+			row$=$('<tr class="table-primary" id="'+data[i].rowid+'"/>');
 		};
 
-		var de_info=transcodeArea(callsign.getAmateurRadioInfoByCallsign(data[i].de));
-		var dx_info=transcodeArea(callsign.getAmateurRadioInfoByCallsign(data[i].dx));
-
-		row$.append($('<td/>').html('<a href="https://www.qrz.com/db/'+data[i].de+ '" target="_blank"><i class="fas fa-search"></i></a><span>&nbsp'+data[i].de+'</span>'));
+		var country=findCountry(countries, data[i].spotdxcc);
+		//var dx_info=transcodeArea(callsign.getAmateurRadioInfoByCallsign(data[i].dx));
+		row$.append($('<td/>').html('<a href="https://www.qrz.com/db/'+data[i].de+ '" target="_blank" rel="noopener"><i class="fas fa-search"></i></a><span>&nbsp'+data[i].de+'</span>'));
+		var freq = Intl.NumberFormat('it-IT', { style: 'decimal' }).format(data[i].freq);
+		row$.append($('<td/>').html('<span class="badge badge-warning" style="width: 65px">'+freq+'</span>'));
+		row$.append($('<td/>').html('<a href="https://www.qrz.com/db/'+data[i].dx+ '" target="_blank" rel="noopener"><i class="fas fa-search"></i></a><span>&nbsp'+data[i].dx+'</span>'));
 		try {
-			row$.append($('<td/>').html('<a href="#" data-toggle="tooltip" title="'+de_info.area+'"><img src="https://www.countryflags.io/'+de_info.areacode+'/shiny/24.png"></a>'));
+  		//	row$.append($('<td/>').html('<a href="#" data-toggle="tooltip" title="'+dx_info.area+'"><img src="https://www.countryflags.io/'+dx_info.areacode+'/shiny/24.png"></a>'));
+  			row$.append($('<td/>').html('<a href="#" data-toggle="tooltip" title="'+country.country+'"><img src="https://www.countryflags.io/'+country.ISO+'/shiny/24.png" alt="'+country.country+'"></a>'));
 		} catch (err) {
 			row$.append($('<td/>'));
 		};
-
-		var freq = data[i].freq.toString().replace(/\./gi, ",");
-		row$.append($('<td/>').html(freq));
-		row$.append($('<td/>').html('<a href="https://www.qrz.com/db/'+data[i].dx+ '" target="_blank"><i class="fas fa-search"></i></a><span>&nbsp'+data[i].dx+'</span>'));
-		try {
-			row$.append($('<td/>').html('<a href="#" data-toggle="tooltip" title="'+dx_info.area+'"><img src="https://www.countryflags.io/'+dx_info.areacode+'/shiny/24.png"></a>'));
-		} catch (err) {
-			row$.append($('<td/>'));
-		};
-		row$.append($('<td/>').html(data[i].comm));
+		row$.append($('<td class="d-none d-lg-table-cell d-xl-table-cell"/>').html(data[i].comm));
 		var dt=new Date(data[i].time * 1000);
 		var hh='00'+dt.getUTCHours();
 		hh=hh.substring(hh.length - 2, hh.length); 
@@ -53,21 +57,76 @@ function buildHtmlTable(selector,data,rl) {
 		var mo='00'+(Number(dt.getUTCMonth())+1);
 		mo=mo.substring(mo.length - 2, mo.length);
 		var yy=dt.getUTCFullYear();
-		dt=hh+':'+mi+' '+dd+'/'+mo+'/'+yy;
-		row$.append($('<td/>').html(dt));
-      		$(selector).append(row$);
-    	}
+		tm=hh+':'+mi;
+		dt=dd+'/'+mo+'/'+yy;
+		row$.append($('<td/>').html(tm));
+		row$.append($('<td class="d-none d-lg-table-cell d-xl-table-cell"/>').html(dt));
+    		$(selector).append(row$);
+   	}
 	return Array.from(myRows);
 };
+function mySearch(event) {
 
+	event.preventDefault();
+	myTimer(); //force the call of query 
+
+};
 
 function myTimer() {
 	var request = new XMLHttpRequest()
 
+	selectedBands = [].map.call(document.getElementById('band').selectedOptions, option => option.value);
+	selectedDEre = [].map.call(document.getElementById('de_re').selectedOptions, option => option.value);
+	selectedDXre = [].map.call(document.getElementById('dx_re').selectedOptions, option => option.value);
+
+	//construct query parameters
+	var qryBands='';
+	var qryAll='';
+	if (selectedBands.length < 14) {
+		qryBands= selectedBands.map(function(el, idx) {
+	    		return 'b=' + el;
+		}).join('&');
+	};
+
+	var qryDEre='';
+	if (selectedDEre.length < 7) {
+		qryDEre = selectedDEre.map(function(el, idx) {
+	    		return 'e=' + el;
+		}).join('&');
+	};
+	if (qryBands && qryDEre){
+		qryAll=qryBands.concat('&'.concat(qryDEre));
+	} else {
+		qryAll=qryBands.concat(qryDEre);
+	};
+
+	var qryDXre='';
+	if (selectedDXre.length< 7) {
+		qryDXre = selectedDXre.map(function(el, idx) {
+	    		return 'x=' + el;
+		}).join('&');
+	};
+
+	if (qryAll && qryDXre) {
+		qryAll=qryAll.concat('&'.concat(qryDXre));
+	} else {
+		qryAll=qryAll.concat(qryDXre);
+	};
+
 	// Open a new connection, using the GET request on the URL endpoint
-	request.open('GET', 'spotlist', true)
+	var qryString='spotlist';
+	if (!!qryAll){
+		qryString=qryString.concat('?'.concat(qryAll));
+	};
+
+	request.open('GET', qryString, true)
+	//when received data, constructs the tables
 	request.onload = function(){
-			rows_list=buildHtmlTable('#bodyspot',JSON.parse(this.response),rows_list );
+		try {
+			rows_list=buildHtmlTable('#bodyspot',JSON.parse(this.response),rows_list,my_countries.country_codes );
+		} catch (err) {
+			console.log(this.response);
 		}
+	}
 	request.send()
 };
--- webapp.py
+++ webapp.py
@@ -1,5 +1,6 @@
+import os
 import flask
-from flask import request, render_template
+from flask import request, render_template, jsonify
 import MySQLdb as my
 import json
 
@@ -10,27 +11,75 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 app.config['SECRET_KEY'] = 'secret!'
 
-with open('config.json') as json_data_file:
+#load config file
+with open('cfg/config.json') as json_data_file:
         cfg = json.load(json_data_file)
+
+#load bands file
+with open('cfg/bands.json') as json_bands:
+        band_frequencies = json.load(json_bands)
+
+#load country file (and send it to front-end)
+def load_country():
+    filename = os.path.join(app.static_folder, 'country.json')
+    with open(filename) as country_file:
+        return json.load(country_file)
+
+#find band in json frequency array
+def find_band_freq(json_object, name):
+    return [obj for obj in json_object if obj['id']==name][0]
 
 def spotquery():
 
-    db = my.connect(host=cfg['mysql']['host'],
+    try:
+        # trancodificare cqdx da regione AF a cq
+        # costruire query con nulla,  spotcq, spotde, band in successione 
+        
+        #get url parameters
+        band=(request.args.getlist('b'))
+        dere=(request.args.getlist('e'))
+        dxre=(request.args.getlist('x'))
+
+        #construct band query decoding frequencis with json file
+        band_qry_string = '(('
+        for i in range(len(band)):
+            freq=find_band_freq(band_frequencies["bands"],band[i])
+            if i > 0:
+                band_qry_string += ') OR ('
+
+            band_qry_string += 'freq BETWEEN ' + str(freq["min"]) + ' AND ' + str(freq["max"])
+
+        band_qry_string += '))'
+        query_string="SELECT rowid, spotter AS de, freq, spotcall AS dx, comment AS comm, time, spotdxcc from dxcluster.spot "                                  
+        if len(band) > 0:
+            query_string += " WHERE "   
+            query_string += band_qry_string
+        
+        query_string += " ORDER BY rowid desc limit 50;"  
+        print query_string
+
+        #connect to db
+        db = my.connect(host=cfg['mysql']['host'],
                     user=cfg['mysql']['user'],
                     passwd=cfg['mysql']['passwd'],
                     db=cfg['mysql']['db']
                     )
-    cursor = db.cursor()
-    number_of_rows = cursor.execute('''SET NAMES 'utf8';''')
-    number_of_rows = cursor.execute('''SELECT rowid, spotter AS de, freq, spotcall AS dx, comment AS comm, time from dxcluster.spot ORDER BY rowid desc limit 50;''')
-    row_headers=[x[0] for x in cursor.description] #this will extract row headers
-    rv=cursor.fetchall()
-    payload=[]
-    for result in rv:
-        payload.append(dict(zip(row_headers,result)))
 
-    db.close()
-    return payload
+        cursor = db.cursor()
+        number_of_rows = cursor.execute('''SET NAMES 'utf8';''')
+        cursor.execute(query_string)
+        row_headers=[x[0] for x in cursor.description] #this will extract row headers
+        rv=cursor.fetchall()
+        payload=[]
+        for result in rv:
+            payload.append(dict(zip(row_headers,result)))
+        cursor.close()
+        return payload
+    except Exception as e:
+        print(e)
+
+    finally:
+        db.close()
 
 @app.route('/spotlist', methods=['GET']) 
 def spotlist():
@@ -40,7 +89,9 @@ def spotlist():
 @app.route('/', methods=['GET']) 
 def spots():
     payload=spotquery()
-    response=flask.Response(render_template('index.html',payload=payload,timer_interval=cfg['timer']['interval']))
+    country_data=load_country()
+    response=flask.Response(render_template('index.html',payload=payload,timer_interval=cfg['timer']['interval'],country_data=country_data))
+    #response=flask.Response(render_template('index.html',payload=payload,timer_interval=cfg['timer']['interval']))
     return response
 
 if __name__ == '__main__':
