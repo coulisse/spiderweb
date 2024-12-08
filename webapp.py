fcb0b6c53ad162b7f3b95c194b3f3827464979b3
coulisse <corrado.gerbaldo@gmail.com>
2024-12-08 09:15:56 +0100

    Lint, Optimization, Telnet, Python 3.13

--- webapp.py
+++ webapp.py
@@ -24,6 +24,7 @@ logger = logging.getLogger(__name__)
 logger.info("Starting SPIDERWEB")
 
 app = flask.Flask(__name__)
+
 app.config["SECRET_KEY"] = secrets.token_hex(16)
 app.config.update(
     SESSION_COOKIE_SECURE=True,
